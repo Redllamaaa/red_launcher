a38d81810a79591920860a11bd112be4d73bf055
Redllamaaa <91138418+Redllamaaa@users.noreply.github.com>
2026-08-22 21:19:12 +0100

    feat(ipcconstants): Removed unused consts

--- src/assets/js/ipcconstants.js
+++ src/assets/js/ipcconstants.js
@@ -3,26 +3,6 @@
 // SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
 export const AZURE_CLIENT_ID = "193694a2-cacf-4a80-a008-6a8b8a358919";
 
-// Opcodes
-export const MSFT_OPCODE = {
-  OPEN_LOGIN: "MSFT_AUTH_OPEN_LOGIN",
-  OPEN_LOGOUT: "MSFT_AUTH_OPEN_LOGOUT",
-  REPLY_LOGIN: "MSFT_AUTH_REPLY_LOGIN",
-  REPLY_LOGOUT: "MSFT_AUTH_REPLY_LOGOUT",
-};
-
-// Reply types for REPLY opcode.
-export const MSFT_REPLY_TYPE = {
-  SUCCESS: "MSFT_AUTH_REPLY_SUCCESS",
-  ERROR: "MSFT_AUTH_REPLY_ERROR",
-};
-
-// Error types for ERROR reply.
-export const MSFT_ERROR = {
-  ALREADY_OPEN: "MSFT_AUTH_ERR_ALREADY_OPEN",
-  NOT_FINISHED: "MSFT_AUTH_ERR_NOT_FINISHED",
-};
-
 export const SHELL_OPCODE = {
   TRASH_ITEM: "TRASH_ITEM",
 };
